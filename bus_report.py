7a1e1a1c771f9ee54604caa20ea81f49ec4a31a9
Atheer-Hr <ath.da96@gmail.com>
2025-10-31 14:29:01 +0300

    Update bus_report.py

--- bus_report.py
+++ bus_report.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlite3
 
-def generate_bus_report(db_path='bus_data.db', output_path='تقرير_الباص.xlsx'):
+def generate_bus_report(db_path='school_system.db', output_path='تقرير_الباص.xlsx'):
     conn = sqlite3.connect(db_path)
     df = pd.read_sql("SELECT * FROM bus_log", conn)
     df.to_excel(output_path, index=False)
